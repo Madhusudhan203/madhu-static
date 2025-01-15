5aebbd090427848adc6400507b95ee6accd4ee07
unknown <a.madhusudhan1783@gmail.com>
2025-01-15 12:34:29 +0530

    commit changes

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function HelloWorld(id) {
   greeting = document.createElement('h3');
   greeting.setAttribute('id', 'hello');
   greeting.style.visibility = "hidden";
-  greeting.innerHTML = "Hello Madhusudhan";
+  greeting.innerHTML = "Hello Madhusudhan!";
 
   element.appendChild(greeting);
 
